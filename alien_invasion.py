ee9d6eb3efd130e18c9d0e3e86b91260c540d704
ssh <ssh@outlook.com>
2019-08-12 16:05:56 +0800

    满屏移动的外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,12 +16,13 @@ def run_game():
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     alien = Alien(ai_settings, screen)
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
